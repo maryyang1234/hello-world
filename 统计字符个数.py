01c946711b18741872f264612e80fe6abdf52e40
maryyang1234 <46594181+maryyang1234@users.noreply.github.com>
2019-04-13 18:51:32 +0800

    Add files via upload

---
+++ 统计字符个数.py
@@ -0,0 +1,27 @@
+
+
+def countChr(str):
+    Upper = 0
+    Lower = 0
+    num = 0
+    spec = 0
+
+    for i in str:
+        if i<= 'Z'and i>= 'A':
+            Upper+=1
+        elif i<='z' and i>='a':
+            Lower+=1
+        elif i<='9'and i >='0':
+            num+=1
+        else:
+            spec +=1
+
+    print("the number of Upper character are : ",Upper)
+    print("the number of the Low character are:",Lower)
+    print("the number of number character are :",num)
+    print("the number of the special character are : ",spec)
+
+str = "#GeeKs01fOr@gEEks07"
+countChr(str)
+
+
